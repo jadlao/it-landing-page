ae8c67bb555708cfd8856a137107885d0c2785da
jadlao <jadlao@gmail.com>
2018-11-03 09:22:59 +1100

    added slick carousel

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,19 @@
+// initializes slick carousel
+$(document).ready(function(){
+    $('.hero-carousel').slick({
+        dots: true,
+        infinite: true,
+        speed: 500,
+        fade: true,
+        cssEase: 'linear',
+        autoplay: true,
+        autoplaySpeed: 5000,
+        arrows: false
+      });
+  });
+
+    
+// opens mobile menu
 function openMenu() {
     //console.log('click');
     var d = document.getElementById('mob-menu');
@@ -5,6 +21,7 @@ function openMenu() {
     d.className += ' open';
 }
 
+// closes mobile menu
 function closeMenu() {
     //console.log('click');
     var d = document.getElementById('mob-menu');
